7534e4c80bc97341e3282aeb7b6077c0baa1b894
Kamikaze <jmagicdev+kamikaze@gmail.com>
2013-05-01 22:26:06 -0400

    Initial commit.

---
+++ server/src/org/rnd/jmagic/webserver/JspServer.java
@@ -0,0 +1,224 @@
+package org.rnd.jmagic.webserver;
+
+import org.rnd.jmagic.engine.*;
+
+public class JspServer
+{
+	private static final java.util.Map<Object, JspServer> _games = new java.util.HashMap<Object, JspServer>();
+
+	private static final Thread cardLoader = new Thread()
+	{
+		@Override
+		public void run()
+		{
+			org.rnd.jmagic.CardLoader.addPackages("org.rnd.jmagic.cards");
+		}
+	};
+
+	{
+		startLoadingCards();
+	}
+
+	public static void startLoadingCards()
+	{
+		synchronized(cardLoader)
+		{
+			if(cardLoader.getState() == java.lang.Thread.State.NEW)
+				cardLoader.start();
+		}
+	}
+
+	public static int numGames()
+	{
+		synchronized(_games)
+		{
+			return _games.size();
+		}
+	}
+
+	public static java.util.UUID createKey()
+	{
+		return java.util.UUID.randomUUID();
+	}
+
+	// Instantiates a two-player game with the current players key and output
+	// writer. Returns a key for use by the second player to join the game.
+	public static java.util.UUID createGame(final java.util.UUID key)
+	{
+		startLoadingCards();
+		while(cardLoader.getState() != java.lang.Thread.State.TERMINATED)
+			try
+			{
+				cardLoader.join(5000);
+			}
+			catch(InterruptedException e)
+			{
+				//
+			}
+
+		final Game game = new Game(GameType.OPEN);
+
+		final java.util.UUID otherKey = createKey();
+		final JspServer server = new JspServer(game);
+
+		final QueueingJSONInterface json = new QueueingJSONInterface();
+		server.addPlayer(key, json);
+		server.addPlayer(otherKey, new QueueingJSONInterface());
+
+		synchronized(_games)
+		{
+			_games.put(key, server);
+			_games.put(otherKey, server);
+		}
+
+		// Adding the interface will immediately ask the player some questions
+		// (name/deck), so put that in another thread so it doesn't hold up this
+		// thread.
+		new Thread()
+		{
+			@Override
+			public void run()
+			{
+				org.rnd.jmagic.engine.PlayerInterface local = json;
+				local = new org.rnd.jmagic.interfaceAdapters.MulticostAdapter(local);
+				// local = new
+				// org.rnd.jmagic.interfaceAdapters.AutomaticPassInterface(local);
+				local = new org.rnd.jmagic.interfaceAdapters.ShortcutInterface(local);
+				local = new org.rnd.jmagic.interfaceAdapters.ManaAbilitiesAdapter(local);
+				game.addInterface(local);
+			}
+		}.start();
+
+		return otherKey;
+	}
+
+	// Joins a game based on a key provided to the player.
+	public static void joinGame(final java.util.UUID key) throws GameDoesNotExistException
+	{
+		final JspServer server;
+
+		synchronized(_games)
+		{
+			if(!_games.containsKey(key))
+				throw new GameDoesNotExistException();
+
+			server = _games.get(key);
+		}
+
+		synchronized(server)
+		{
+			final Game game = server.getGame();
+
+			// Add the interface and start the game in another thread so that we
+			// can complete the joinGame request, while the game asks the player
+			// for their name/deck/etc.
+			Thread gameThread = new Thread(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					QueueingJSONInterface json = server.getPlayer(key);
+					org.rnd.jmagic.engine.PlayerInterface local = json;
+					local = new org.rnd.jmagic.interfaceAdapters.MulticostAdapter(local);
+					// local = new
+					// org.rnd.jmagic.interfaceAdapters.AutomaticPassInterface(local);
+					local = new org.rnd.jmagic.interfaceAdapters.ShortcutInterface(local);
+					local = new org.rnd.jmagic.interfaceAdapters.ManaAbilitiesAdapter(local);
+					game.addInterface(local);
+
+					game.run();
+				}
+			});
+
+			server.setGameThread(gameThread);
+			gameThread.start();
+		}
+	}
+
+	public static java.util.UUID getOtherKey(java.util.UUID key)
+	{
+		synchronized(_games)
+		{
+			if(_games.containsKey(key))
+			{
+				JspServer server = _games.get(key);
+				synchronized(server)
+				{
+					return server.getOtherPlayer(key);
+				}
+			}
+		}
+		return null;
+	}
+
+	public static void clearAllGames()
+	{
+		synchronized(_games)
+		{
+			for(JspServer server: _games.values())
+				synchronized(server)
+				{
+					if(server.getGameThread() != null)
+						server.getGameThread().interrupt();
+				}
+			_games.clear();
+		}
+	}
+
+	public static JspServer getServer(java.util.UUID key)
+	{
+		synchronized(_games)
+		{
+			return _games.get(key);
+		}
+	}
+
+	public static class GameDoesNotExistException extends Exception
+	{
+		private static final long serialVersionUID = 1L;
+	}
+
+	private org.rnd.jmagic.engine.Game _game = null;
+	private java.util.Map<java.util.UUID, QueueingJSONInterface> _players;
+	private Thread _gameThread = null;
+
+	public JspServer(Game game)
+	{
+		this._game = game;
+		this._players = new java.util.HashMap<java.util.UUID, QueueingJSONInterface>();
+		this._gameThread = null;
+	}
+
+	public Game getGame()
+	{
+		return this._game;
+	}
+
+	public void setGameThread(Thread gameThread)
+	{
+		this._gameThread = gameThread;
+	}
+
+	public Thread getGameThread()
+	{
+		return this._gameThread;
+	}
+
+	public void addPlayer(java.util.UUID key, QueueingJSONInterface player)
+	{
+		this._players.put(key, player);
+	}
+
+	public QueueingJSONInterface getPlayer(java.util.UUID key)
+	{
+		return this._players.get(key);
+	}
+
+	public java.util.UUID getOtherPlayer(java.util.UUID key)
+	{
+		for(java.util.UUID playerKey: this._players.keySet())
+			if(!playerKey.equals(key))
+				return playerKey;
+		return null;
+	}
+}
---
+++ server/src/org/rnd/jmagic/webserver/QueueingJSONInterface.java
@@ -0,0 +1,405 @@
+package org.rnd.jmagic.webserver;
+
+import java.io.*;
+import java.util.*;
+
+import org.rnd.jmagic.engine.*;
+import org.rnd.jmagic.sanitized.*;
+import org.rnd.util.*;
+
+import com.google.gson.reflect.*;
+import com.google.gson.stream.*;
+
+public class QueueingJSONInterface implements org.rnd.jmagic.engine.PlayerInterface
+{
+	public static final class SanitizedGameStateAdapter extends com.google.gson.TypeAdapter<SanitizedGameState>
+	{
+		public static final com.google.gson.TypeAdapterFactory FACTORY = new com.google.gson.TypeAdapterFactory()
+		{
+			@Override
+			@SuppressWarnings("unchecked")
+			// we use a runtime check to make sure the 'T's equal
+			public <T> com.google.gson.TypeAdapter<T> create(com.google.gson.Gson gson, TypeToken<T> typeToken)
+			{
+				return typeToken.getRawType() == SanitizedGameState.class ? (com.google.gson.TypeAdapter<T>)new SanitizedGameStateAdapter(gson) : null;
+			}
+		};
+
+		private com.google.gson.Gson gson;
+
+		public SanitizedGameStateAdapter(com.google.gson.Gson gson)
+		{
+			this.gson = gson;
+		}
+
+		@Override
+		public void write(JsonWriter out, SanitizedGameState value) throws IOException
+		{
+			if(value == null)
+			{
+				out.nullValue();
+				return;
+			}
+
+			out.beginObject();
+
+			this.gson.getAdapter(new com.google.gson.reflect.TypeToken<java.util.Map<Integer, SanitizedIdentified>>()
+			{
+				//
+			}).write(out.name("actual"), value);
+
+			out.name("players").beginArray();
+			for(Integer player: value.players)
+				out.value(player);
+			out.endArray();
+
+			out.name("battlefield").value(value.battlefield);
+			out.name("commandZone").value(value.commandZone);
+			out.name("exileZone").value(value.exileZone);
+			out.name("stack").value(value.stack);
+			out.name("turn").value(value.turn);
+
+			out.name("phase");
+			if(value.phase == null)
+				out.nullValue();
+			else
+				out.value(value.phase.name());
+
+			out.name("step");
+			if(value.step == null)
+				out.nullValue();
+			else
+				out.value(value.step.name());
+
+			out.endObject();
+		}
+
+		@Override
+		public SanitizedGameState read(JsonReader in) throws IOException
+		{
+			return null;
+		}
+
+	}
+
+	@SuppressWarnings("unused")
+	private static class DivideWrapper
+	{
+		private String func = "divide";
+		private int quantity;
+		private int minimum;
+		private String beingDivided;
+		private List<SanitizedTarget> targets;
+
+		private DivideWrapper(int _minimum, int _quantity, String _beingDivided, List<SanitizedTarget> _targets)
+		{
+			this.quantity = _quantity;
+			this.minimum = _minimum;
+			this.beingDivided = _beingDivided;
+			this.targets = _targets;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class ChooseNumberWrapper
+	{
+		private String func = "chooseNumber";
+		private NumberRange range;
+		private String description;
+
+		private ChooseNumberWrapper(NumberRange _range, String _description)
+		{
+			this.range = _range;
+			this.description = _description;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class ChooseWrapper<T extends Serializable>
+	{
+		private String func = "choose";
+		private ChooseParameters<T> parameterObject;
+
+		private ChooseWrapper(ChooseParameters<T> _parameterObject)
+		{
+			this.parameterObject = _parameterObject;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class AlertWaitingWrapper
+	{
+		private String func = "alertWaiting";
+		private SanitizedPlayer who;
+
+		private AlertWaitingWrapper(SanitizedPlayer _who)
+		{
+			this.who = _who;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class AlertStateReversionWrapper
+	{
+		private String func = "alertStateReversion";
+		private ReversionParameters parameters;
+
+		private AlertStateReversionWrapper(ReversionParameters _parameters)
+		{
+			this.parameters = _parameters;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class AlertStateWrapper
+	{
+		private String func = "alertState";
+		private SanitizedGameState sanitizedGameState;
+
+		private AlertStateWrapper(SanitizedGameState _sanitizedGameState)
+		{
+			this.sanitizedGameState = _sanitizedGameState;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class AlertEventWrapper
+	{
+		private String func = "alertEvent";
+		private SanitizedEvent event;
+
+		private AlertEventWrapper(SanitizedEvent _event)
+		{
+			this.event = _event;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class AlertChoiceWrapper
+	{
+		private String func = "alertChoice";
+		private int playerID;
+		private ChooseParameters<?> choice;
+
+		private AlertChoiceWrapper(ChooseParameters<?> _choice, int _playerID)
+		{
+			this.playerID = _playerID;
+			this.choice = _choice;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class AlertErrorWrapper
+	{
+		private String func = "alertError";
+		private String errorType;
+		private ErrorParameters parameters;
+
+		private AlertErrorWrapper(ErrorParameters _parameters)
+		{
+			this.errorType = _parameters.getClass().getSimpleName();
+			this.parameters = _parameters;
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private static class GetDeckWrapper
+	{
+		private String func = "getDeck";
+	}
+
+	@SuppressWarnings("unused")
+	private static class GetNameWrapper
+	{
+		private String func = "getName";
+	}
+
+	@SuppressWarnings("unused")
+	private static class SetPlayerIDWrapper
+	{
+		private String func = "setPlayerID";
+		private int playerID;
+
+		private SetPlayerIDWrapper(int _playerID)
+		{
+			this.playerID = _playerID;
+		}
+	}
+
+	private static final java.lang.reflect.Type LIST_INTEGER_TYPE = new TypeToken<List<Integer>>()
+	{
+		//
+	}.getType();
+
+	private com.google.gson.Gson gson;
+	private java.util.concurrent.BlockingQueue<String> messageQueue;
+	private java.util.concurrent.BlockingQueue<String> responseQueue;
+
+	public QueueingJSONInterface()
+	{
+		com.google.gson.GsonBuilder builder = new com.google.gson.GsonBuilder();
+		builder.registerTypeAdapterFactory(SanitizedGameStateAdapter.FACTORY);
+
+		this.gson = builder.create();
+		this.messageQueue = new java.util.concurrent.LinkedBlockingDeque<String>();
+		this.responseQueue = new java.util.concurrent.SynchronousQueue<String>(true);
+	}
+
+	@Override
+	public void alertChoice(final int _playerID, final ChooseParameters<?> _choice)
+	{
+		String json = this.gson.toJson(new AlertChoiceWrapper(_choice, _playerID));
+		this.messageQueue.add(json);
+	}
+
+	@Override
+	public void alertError(final ErrorParameters _parameters)
+	{
+		String json = this.gson.toJson(new AlertErrorWrapper(_parameters));
+		this.messageQueue.add(json);
+	}
+
+	@Override
+	public void alertEvent(final SanitizedEvent _event)
+	{
+		String json = this.gson.toJson(new AlertEventWrapper(_event));
+		this.messageQueue.add(json);
+	}
+
+	@Override
+	public void alertState(final SanitizedGameState _sanitizedGameState)
+	{
+		String json = this.gson.toJson(new AlertStateWrapper(_sanitizedGameState));
+		this.messageQueue.add(json);
+	}
+
+	@Override
+	public void alertStateReversion(final ReversionParameters _parameters)
+	{
+		String json = this.gson.toJson(new AlertStateReversionWrapper(_parameters));
+		this.messageQueue.add(json);
+	}
+
+	@Override
+	public void alertWaiting(final SanitizedPlayer _who)
+	{
+		String json = this.gson.toJson(new AlertWaitingWrapper(_who));
+		this.messageQueue.add(json);
+	}
+
+	@Override
+	public <T extends Serializable> List<Integer> choose(final ChooseParameters<T> _parameterObject)
+	{
+		String json = this.gson.toJson(new ChooseWrapper<T>(_parameterObject));
+		this.messageQueue.add(json);
+
+		try
+		{
+			json = this.responseQueue.take();
+			return this.gson.fromJson(json, LIST_INTEGER_TYPE);
+		}
+		catch(InterruptedException e)
+		{
+			return null;
+		}
+	}
+
+	@Override
+	public int chooseNumber(final NumberRange _range, final String _description)
+	{
+		String json = this.gson.toJson(new ChooseNumberWrapper(_range, _description));
+		this.messageQueue.add(json);
+
+		try
+		{
+			json = this.responseQueue.take();
+			return this.gson.fromJson(json, int.class);
+		}
+		catch(InterruptedException e)
+		{
+			return 0;
+		}
+	}
+
+	@Override
+	public void divide(final int _quantity, final int _minimum, final int _whatFrom, final String _beingDivided, final List<SanitizedTarget> _targets)
+	{
+		String json = this.gson.toJson(new DivideWrapper(_minimum, _quantity, _beingDivided, _targets));
+		this.messageQueue.add(json);
+	}
+
+	@Override
+	public Deck getDeck()
+	{
+		String json = this.gson.toJson(new GetDeckWrapper());
+		this.messageQueue.add(json);
+
+		try
+		{
+			json = this.responseQueue.take();
+			String[] deck = this.gson.fromJson(json, new com.google.gson.reflect.TypeToken<String[]>()
+			{
+				//
+			}.getType());
+
+			return new Deck(Arrays.asList(deck), new java.util.LinkedList<String>());
+		}
+		catch(InterruptedException e)
+		{
+			return new Deck();
+		}
+	}
+
+	@Override
+	public String getName()
+	{
+		String json = this.gson.toJson(new GetNameWrapper());
+		this.messageQueue.add(json);
+
+		try
+		{
+			json = this.responseQueue.take();
+			json = this.gson.fromJson(json, String.class);
+			return json;
+		}
+		catch(InterruptedException e)
+		{
+			return "Player";
+		}
+
+	}
+
+	@Override
+	public void setPlayerID(int playerID)
+	{
+		String json = this.gson.toJson(new SetPlayerIDWrapper(playerID));
+		this.messageQueue.add(json);
+	}
+
+	public String getNextMessage(int timeout, java.util.concurrent.TimeUnit unit)
+	{
+		try
+		{
+			String poll = this.messageQueue.poll(timeout, unit);
+			// Remove any consecutive identical messages
+			if(poll != null)
+			{
+				while(poll.equals(this.messageQueue.peek()))
+					this.messageQueue.poll();
+				java.util.logging.Logger.getAnonymousLogger().log(java.util.logging.Level.INFO, "Send Message: " + poll);
+			}
+			return poll;
+		}
+		catch(InterruptedException e)
+		{
+			return null;
+		}
+	}
+
+	public void registerResponse(String response)
+	{
+		java.util.logging.Logger.getAnonymousLogger().log(java.util.logging.Level.INFO, "Register Response: " + response);
+		this.responseQueue.add(response);
+	}
+
+}
